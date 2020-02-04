1af9fcfdd40d044df25caab0cf884a6af0505d1e
seifalaa <seifalaa143@gmail.com>
2020-02-04 17:45:32 +0200

    enhacing

--- top categories of the week/main.js
+++ top categories of the week/main.js
@@ -1,95 +1,141 @@
-function createGrid() {
-    var rows = document.getElementById("numRows").value;
-    var cols = document.getElementById("numCols").value;
-    var gridSize = rows * cols;
-    var picLinks = $("#picLinks").val().split("\n");
-    var listData = $("#listsData").val().split("\n");
-
-    var container = document.createElement("div");
-    container.className = "container-fluid bounceInUp";
-
-    var heading = document.createElement("h1");
-    heading.className = "alves flash";
-
-    var headingText = document.createTextNode("TOP BRANDS OF THE WEEK");
-
-    heading.appendChild(headingText);
-    container.appendChild(heading);
-
-    for (var i = 0; i < rows; i++) {
+$(document).ready(function () {
+    $("#container").hide();
+    $("#btn2").hide();
+    $("#btn3").hide();
+    $(".form-group.t1").hide();
+    $("#btn1").click(function () {
 
 
-        var row = document.createElement("div");
-        row.className = "row m-0 p-0 bounceInUp";
+        gridSize = $("#numRows").val() * $("#numCols").val();
 
+        if (gridSize != 0) {
+            var label = document.createElement("label");
+            var text = document.createTextNode("Enter products thumbnails links: ");
+            label.append(text);
+            $("#container").prepend(label);
+            for (var i = 0; i < gridSize; i++) {
+                var formGroup = document.createElement("div");
+                formGroup.className = "form-group txtField";
+                var field = document.createElement("input");
+                field.className = "form-control txtField";
+                field.placeholder = "Enter link";
+                field.setAttribute("required", "required");
 
+                formGroup.append(field);
+                $("#container").append(formGroup);
 
-        var counter = 1;
-        for (var j = 0; j < cols; j++) {
-
-
-            var col = document.createElement("div");
-            col.className = "col-lg-" + 12 / cols + " col-md-" + 12 / cols + " col-sm-" + 12 / cols + " col-" + 12 / cols + "  m-0";
-            col.id = "One";
-
-            var innerRow = document.createElement("div");
-            innerRow.className = "row m-0 p-0";
-            innerRow.id = "cell";
-
-            var imgCol = document.createElement("div");
-            imgCol.className = "col-lg-5 col-md-12 col-sm-12 col-12  m-0 d-flex justify-content-center";
-
-            var img = document.createElement("img");
-            img.className = "img-fluid";
-            img.src = picLinks[j];
-
-            var listCol = document.createElement("div");
-            listCol.className = "col-lg-7 col-md-12 col-sm-12 col-12  m-0";
-
-            var listHeading = document.createElement("h6");
-
-            var listHeadingText = document.createTextNode(listData[j * 6]);
-
-            var list = document.createElement("ul");
-            list.className = "list-group";
-
-            for (var k = 0; k < 5; k++) {
-
-                var listItem = document.createElement("li");
-                listItem.className = "list-group-item";
-                var itemText = document.createTextNode(listData[counter]);
-                listItem.appendChild(itemText);
-                counter++;
-                list.appendChild(listItem);
+                var col = document.createElement("div");
+                col.className = "col-" + 12 / gridSize + "px-2";
+                var field2 = document.createElement("textarea");
+                field2.className = "form-control txtArea";
+                field2.placeholder = "Enter Items";
+                field2.setAttribute("required", "required");
+                formGroup = document.createElement("div");
+                formGroup.className = "form-group px-2";
+                formGroup.append(field2);
+                col.append(formGroup);
+                $("#row1").append(col);
             }
-
-            counter++;
-
-            listHeading.appendChild(listHeadingText);
+            $("#container").show();
 
 
-            listCol.appendChild(listHeading);
-            listCol.appendChild(list);
 
-
-            imgCol.appendChild(img);
-            innerRow.appendChild(imgCol);
-            innerRow.appendChild(listCol);
-
-            col.appendChild(innerRow);
-
-            row.appendChild(col);
-
+            $("#btn2").show();
+            $(".form-group.t1").show();
         }
 
-        container.appendChild(row);
 
-        var parent = document.getElementById("con");
-        parent.parentNode.removeChild(parent);
+    });
+    $("#form3").submit(function () {
 
-        document.body.appendChild(container);
+        var cols = $("#numCols").val();
+        var rows = $("#numRows").val();
+        gridSize = $("#numRows").val() * $("#numCols").val();
+        var links = $(".form-control.txtField");
+
+        var items = $(".form-control.txtArea");
+        var itemsVal = [];
+        for (var i = 0; i < items.length; i++) {
+            itemsVal[i] = $(items[i]).val().split("\n");
+        }
+        var container = document.createElement("div");
+        container.className = "container-fluid bounceInUp";
+
+        var heading = document.createElement("h1");
+        heading.className = "alves flash";
+
+        var headingText = document.createTextNode("TOP BRANDS OF THE WEEK");
+
+        heading.append(headingText);
+        container.append(heading);
+
+        var linkCounter = 0;
+        var itemsCounter = 0;
+        var itemsCounter1 = 0;
+        for (var i = 0; i < rows; i++) {
+            var row = document.createElement("div");
+            row.className = "row m-0 p-0 bounceInUp";
+
+            for (var j = 0; j < cols; j++) {
+
+                var col = document.createElement("div");
+                col.className = "col-lg-" + 12 / cols + " col-md-" + 12 / cols + " col-sm-" + 12 / cols + " col-" + 12 / cols + "  m-0";
 
 
-    }
+                var innerRow = document.createElement("div");
+                innerRow.className = "row m-0 p-0";
+                innerRow.id = "cell";
 
-}
+                var imgCol = document.createElement("div");
+                imgCol.className = "col-lg-5 col-md-12 col-sm-12 col-12  m-0 d-flex justify-content-center";
+
+                var img = document.createElement("img");
+                img.className = "img-fluid";
+
+                img.src = $(links[linkCounter++]).val();
+
+                var listCol = document.createElement("div");
+                listCol.className = "col-lg-7 col-md-12 col-sm-12 col-12  m-0";
+
+                var listHeading = document.createElement("h6");
+
+                var listHeadingText = document.createTextNode(itemsVal[itemsCounter++][0]);
+
+
+                var list = document.createElement("ul");
+                list.className = "list-group";
+
+                for (var k = 1; k < itemsVal[itemsCounter1].length; k++) {
+
+
+                    var listItem = document.createElement("li");
+                    listItem.className = "list-group-item";
+                    var itemText = document.createTextNode(itemsVal[itemsCounter1][k]);
+                    listItem.append(itemText);
+                    list.append(listItem);
+                }
+                itemsCounter1++;
+                listHeading.append(listHeadingText);
+
+
+                listCol.append(listHeading);
+                listCol.append(list);
+
+                imgCol.append(img);
+                innerRow.append(imgCol);
+                innerRow.append(listCol);
+                col.append(innerRow);
+
+                row.append(col);
+
+            }
+
+            container.append(row);
+
+
+            $("#con").empty();
+            $("#con").append(container);
+
+        }
+    });
+});
